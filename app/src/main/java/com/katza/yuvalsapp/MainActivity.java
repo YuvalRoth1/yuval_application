4b7d1791786817503ea387f735d1130a628c874f
sapir <sapir.daniel1010@gmail.com>
2025-12-31 14:28:23 +0200

    options & context menu

--- app/src/main/java/com/katza/yuvalsapp/MainActivity.java
+++ app/src/main/java/com/katza/yuvalsapp/MainActivity.java
@@ -1,10 +1,15 @@
 package com.katza.yuvalsapp;
 
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.SeekBar;
 import android.widget.Switch;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -59,13 +64,60 @@ public class MainActivity extends AppCompatActivity {
             public void onStopTrackingTouch(SeekBar seekBar) {}
         });
 
-        // התחל את ה-ImageView כנסתר
         like.setVisibility(View.INVISIBLE);
     }
 
     private void unitViews() {
         switch1 = findViewById(R.id.switch1);
         like = findViewById(R.id.like);
-        bar = findViewById(R.id.Bar);  // חשוב! עכשיו SeekBar מאותחל
+        registerForContextMenu(like);
+        bar = findViewById(R.id.Bar);
+
+
+
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item){
+        super.onOptionsItemSelected(item);
+        int id = item.getItemId();
+
+        if(id == R.id.action_page1){
+            Toast.makeText(this, "you selected page 1", Toast.LENGTH_SHORT).show();
+        }
+
+        else if(id == R.id.action_page2){
+            Toast.makeText(this, "you selected page 2", Toast.LENGTH_SHORT).show();
+        }
+
+        return true;
+    }
+
+    @Override
+    public void onCreateContextMenu (ContextMenu menu, View v,ContextMenu.ContextMenuInfo menuInfo){
+        super.onCreateContextMenu(menu,v,menuInfo);
+
+        MenuInflater  inflater = getMenuInflater();
+        inflater.inflate(R.menu.context_menu, menu);
+    }
+
+    public boolean onContextItemSelected(MenuItem item){
+
+        if(item.getItemId()==R.id.action_option1){
+            Toast.makeText(this, "you selected first item", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+
+        else if(item.getItemId()==R.id.action_option2){
+            Toast.makeText(this, "you selected second item", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        return false;
+    }
+
+
 }

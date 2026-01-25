a1fc5c9b2fcf948e59d123813879915008c1e06b
YuvalRoth1 <yuval.shalom.roth@gmail.com>
2026-01-25 16:33:43 +0200

    Camera Intent

---
+++ app/src/main/java/com/katza/yuvalsapp/Camera.java
@@ -0,0 +1,59 @@
+package com.katza.yuvalsapp;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Camera extends AppCompatActivity {
+
+    ImageView imageView;
+    Button btnCamera;
+
+    ActivityResultLauncher<Intent> cameraLauncher =
+            registerForActivityResult(
+                    new ActivityResultContracts.StartActivityForResult(),
+                    result -> {
+                        if (result.getResultCode() == RESULT_OK && result.getData() != null) {
+                            Bundle extras = result.getData().getExtras();
+                            if (extras != null) {
+                                Bitmap imageBitmap = (Bitmap) extras.get("data");
+                                imageView.setImageBitmap(imageBitmap);
+                            }
+                        } else {
+                            Toast.makeText(this, "צילום בוטל", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_camera);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        imageView = findViewById(R.id.imageView);
+        btnCamera = findViewById(R.id.btnCamera);
+
+        btnCamera.setOnClickListener(v -> {
+            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            cameraLauncher.launch(intent);
+        });
+    }
+}
